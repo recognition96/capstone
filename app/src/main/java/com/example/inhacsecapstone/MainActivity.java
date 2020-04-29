f18ccbaca589b2f089ed71b68becbac139aaa752
esgyu <isalpha@naver.com>
2020-04-29 22:26:40 +0900

    Bottom Navigation Revise#1

--- app/src/main/java/com/example/inhacsecapstone/MainActivity.java
+++ app/src/main/java/com/example/inhacsecapstone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.inhacsecapstone;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -18,32 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                switch(item.getItemId()) {
-                    case R.id.calendar:
-                        Calendar calendar = new Calendar();
-                        fragmentTransaction.replace(R.id.fragment,calendar);
-                        break;
-                    case R.id.camera:
-                        Camera camera = new Camera();
-                        fragmentTransaction.replace(R.id.fragment,camera);
-                        break;
-                    case R.id.userdrug:
-                        Userdrug userdrug = new Userdrug();
-                        fragmentTransaction.replace(R.id.fragment,userdrug);
-                        break;
-                }
-                fragmentTransaction.commit();
-                return true;
-            }
-        });
-
-        bottomNavigationView.setSelectedItemId(R.id.calendar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,new Calendar()).commit();
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch(item.getItemId()) {
+                case R.id.calendar:
+                    selectedFragment = new Calendar();
+                    break;
+                case R.id.camera:
+                    selectedFragment = new Camera();
+                    break;
+                case R.id.userdrug:
+                    selectedFragment = new Userdrug();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment,selectedFragment).commit();
+            return true;
+        }
+    };
 
 }
