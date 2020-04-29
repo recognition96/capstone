02d3abfa66bdb3fbf3b3295ac218a1f1461241ee
esgyu <isalpha@naver.com>
2020-04-29 21:27:09 +0900

    Bottom Navigation

--- app/src/main/java/com/example/inhacsecapstone/MainActivity.java
+++ app/src/main/java/com/example/inhacsecapstone/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.inhacsecapstone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                switch(item.getItemId()) {
+                    case R.id.calendar:
+                        Calendar calendar = new Calendar();
+                        fragmentTransaction.replace(R.id.fragment,calendar);
+                        break;
+                    case R.id.camera:
+                        Camera camera = new Camera();
+                        fragmentTransaction.replace(R.id.fragment,camera);
+                        break;
+                    case R.id.userdrug:
+                        Userdrug userdrug = new Userdrug();
+                        fragmentTransaction.replace(R.id.fragment,userdrug);
+                        break;
+                }
+                fragmentTransaction.commit();
+                return true;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.calendar);
     }
-    // 수정합니다. by 이상규
-    // 수정합니다. 2 by 이상규
+
+
 }
