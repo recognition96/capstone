d0132765bea4b2b1694fffa33712c628cff06751
recognition96 <dlstlr28@gmail.com>
2020-05-15 01:55:33 +0900

    Arranging code (1)  - delete Filters

--- cameraview/src/main/java/com/otaliastudios/cameraview/filter/Filters.java
+++ cameraview/src/main/java/com/otaliastudios/cameraview/filter/Filters.java
@@ -2,28 +2,6 @@ package com.otaliastudios.cameraview.filter;
 
 import androidx.annotation.NonNull;
 
-import com.otaliastudios.cameraview.filters.AutoFixFilter;
-import com.otaliastudios.cameraview.filters.BlackAndWhiteFilter;
-import com.otaliastudios.cameraview.filters.BrightnessFilter;
-import com.otaliastudios.cameraview.filters.ContrastFilter;
-import com.otaliastudios.cameraview.filters.CrossProcessFilter;
-import com.otaliastudios.cameraview.filters.DocumentaryFilter;
-import com.otaliastudios.cameraview.filters.DuotoneFilter;
-import com.otaliastudios.cameraview.filters.FillLightFilter;
-import com.otaliastudios.cameraview.filters.GammaFilter;
-import com.otaliastudios.cameraview.filters.GrainFilter;
-import com.otaliastudios.cameraview.filters.GrayscaleFilter;
-import com.otaliastudios.cameraview.filters.HueFilter;
-import com.otaliastudios.cameraview.filters.InvertColorsFilter;
-import com.otaliastudios.cameraview.filters.LomoishFilter;
-import com.otaliastudios.cameraview.filters.PosterizeFilter;
-import com.otaliastudios.cameraview.filters.SaturationFilter;
-import com.otaliastudios.cameraview.filters.SepiaFilter;
-import com.otaliastudios.cameraview.filters.SharpnessFilter;
-import com.otaliastudios.cameraview.filters.TemperatureFilter;
-import com.otaliastudios.cameraview.filters.TintFilter;
-import com.otaliastudios.cameraview.filters.VignetteFilter;
-
 /**
  * Contains commonly used {@link Filter}s.
  *
@@ -33,70 +11,7 @@ import com.otaliastudios.cameraview.filters.VignetteFilter;
 public enum Filters {
 
     /** @see NoFilter */
-    NONE(NoFilter.class),
-
-    /** @see AutoFixFilter */
-    AUTO_FIX(AutoFixFilter.class),
-
-    /** @see BlackAndWhiteFilter */
-    BLACK_AND_WHITE(BlackAndWhiteFilter.class),
-
-    /** @see BrightnessFilter */
-    BRIGHTNESS(BrightnessFilter.class),
-
-    /** @see ContrastFilter */
-    CONTRAST(ContrastFilter.class),
-
-    /** @see CrossProcessFilter */
-    CROSS_PROCESS(CrossProcessFilter.class),
-
-    /** @see DocumentaryFilter */
-    DOCUMENTARY(DocumentaryFilter.class),
-
-    /** @see DuotoneFilter */
-    DUOTONE(DuotoneFilter.class),
-
-    /** @see FillLightFilter */
-    FILL_LIGHT(FillLightFilter.class),
-
-    /** @see GammaFilter */
-    GAMMA(GammaFilter.class),
-
-    /** @see GrainFilter */
-    GRAIN(GrainFilter.class),
-
-    /** @see GrayscaleFilter */
-    GRAYSCALE(GrayscaleFilter.class),
-
-    /** @see HueFilter */
-    HUE(HueFilter.class),
-
-    /** @see InvertColorsFilter */
-    INVERT_COLORS(InvertColorsFilter.class),
-
-    /** @see LomoishFilter */
-    LOMOISH(LomoishFilter.class),
-
-    /** @see PosterizeFilter */
-    POSTERIZE(PosterizeFilter.class),
-
-    /** @see SaturationFilter */
-    SATURATION(SaturationFilter.class),
-
-    /** @see SepiaFilter */
-    SEPIA(SepiaFilter.class),
-
-    /** @see SharpnessFilter */
-    SHARPNESS(SharpnessFilter.class),
-
-    /** @see TemperatureFilter */
-    TEMPERATURE(TemperatureFilter.class),
-
-    /** @see TintFilter */
-    TINT(TintFilter.class),
-
-    /** @see VignetteFilter */
-    VIGNETTE(VignetteFilter.class);
+    NONE(NoFilter.class);
 
     private Class<? extends Filter> filterClass;
 
