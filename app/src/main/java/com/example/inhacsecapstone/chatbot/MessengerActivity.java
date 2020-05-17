0706e7644c4a5b7b660a91f3391a5adb19c1d2f5
esgyu <isalpha@naver.com>
2020-05-17 15:18:56 +0900

    Camera View Http 통신을 UI Thread에서 하면 Fatal Error발생 새로운 Thread로 통신하게끔 변경 및 httpconnection 클래스의 singleton 객체로 주소변경

--- app/src/main/java/com/example/inhacsecapstone/chatbot/MessengerActivity.java
+++ app/src/main/java/com/example/inhacsecapstone/chatbot/MessengerActivity.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -25,6 +26,7 @@ import androidx.core.content.ContextCompat;
 
 import com.example.inhacsecapstone.R;
 import com.example.inhacsecapstone.drugs.Recog.RecogResultActivity;
+import com.example.inhacsecapstone.serverconnect.HttpConnection;
 import com.github.bassaer.chatmessageview.model.IChatUser;
 import com.github.bassaer.chatmessageview.model.Message;
 import com.github.bassaer.chatmessageview.view.ChatView;
@@ -47,33 +49,23 @@ import static android.speech.tts.TextToSpeech.ERROR;
 
 public class MessengerActivity extends Activity {
 
-    @VisibleForTesting
-    protected static final int RIGHT_BUBBLE_COLOR = R.color.colorPrimaryDark;
-    @VisibleForTesting
-    protected static final int SEND_ICON = R.drawable.ic_action_send;
-    @VisibleForTesting
-    protected static final int RIGHT_MESSAGE_TEXT_COLOR = Color.WHITE;
-    @VisibleForTesting
-    protected static final int LEFT_MESSAGE_TEXT_COLOR = Color.BLACK;
-    @VisibleForTesting
-    protected static final String INPUT_TEXT_HINT = "메시지를 입력하세요.";
-    @VisibleForTesting
-    protected static final int MESSAGE_MARGIN = 5;
-    private static final int READ_REQUEST_CODE = 100;
-
-    private static final String portNumber = "5000";
-    private static final String ipv4Address = "118.47.168.30";
-    private Context context;
     private Intent SttIntent;
     private SpeechRecognizer mRecognizer;
     private TextToSpeech tts;
-
+    private ChatView mChatView;
+    private ArrayList<User> mUsers;
+    private HttpConnection httpConn = HttpConnection.getInstance();
     public void setColors() {
-        mChatView.setRightBubbleColor(ContextCompat.getColor(context, RIGHT_BUBBLE_COLOR));
-        mChatView.setLeftBubbleColor(ContextCompat.getColor(context, R.color.colorPrimaryDark));
-        mChatView.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
-        mChatView.setSendButtonColor(ContextCompat.getColor(context, R.color.colorAccent));
-
+        int RIGHT_BUBBLE_COLOR = R.color.colorPrimaryDark;
+        int SEND_ICON = R.drawable.ic_action_send;
+        int RIGHT_MESSAGE_TEXT_COLOR = Color.WHITE;
+        int LEFT_MESSAGE_TEXT_COLOR = Color.BLACK;
+        String INPUT_TEXT_HINT = "메시지를 입력하세요.";
+        int MESSAGE_MARGIN = 5;
+        mChatView.setRightBubbleColor(ContextCompat.getColor(getApplicationContext(), RIGHT_BUBBLE_COLOR));
+        mChatView.setLeftBubbleColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
+        mChatView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+        mChatView.setSendButtonColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
         mChatView.setSendIcon(SEND_ICON);
         mChatView.setOptionIcon(R.drawable.ic_mic_black_24dp);
         mChatView.setOptionButtonColor(R.color.colorAccent);
@@ -97,55 +89,18 @@ public class MessengerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messenger);
-        context = this.getApplicationContext();
         initUsers();
-
         // 화면 생성 시 Welcome Message 출력
-        connectServerSendText("안녕");
+        sendTextToServer("안녕");
         //
-
         mChatView = findViewById(R.id.chat_view);
         setColors();
-        mChatView.setOnBubbleClickListener(new Message.OnBubbleClickListener() {
-            @Override
-            public void onClick(Message message) {
-                mChatView.updateMessageStatus(message, MyMessageStatusFormatter.STATUS_SEEN);
-                Toast.makeText(
-                        MessengerActivity.this,
-                        "click : " + message.getUser().getName() + " - " + message.getText(),
-                        Toast.LENGTH_SHORT
-                ).show();
-            }
-        });
-
-        mChatView.setOnIconClickListener(new Message.OnIconClickListener() {
-            @Override
-            public void onIconClick(Message message) {
-                Toast.makeText(
-                        MessengerActivity.this,
-                        "click : icon " + message.getUser().getName(),
-                        Toast.LENGTH_SHORT
-                ).show();
-            }
-        });
-
-        mChatView.setOnIconLongClickListener(new Message.OnIconLongClickListener() {
-            @Override
-            public void onIconLongClick(Message message) {
-                Toast.makeText(
-                        MessengerActivity.this,
-                        "Removed this message \n" + message.getText(),
-                        Toast.LENGTH_SHORT
-                ).show();
-                mChatView.getMessageView().remove(message);
-            }
-        });
 
         //Click Send Button
         mChatView.setOnClickSendButtonListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!mChatView.getInputText().equals("")) {
+                if(!mChatView.getInputText().equals("")) {
                     Message message = new Message.Builder()
                             .setUser(mUsers.get(0))
                             .setRight(true)
@@ -157,7 +112,7 @@ public class MessengerActivity extends Activity {
                             .setStatus(MyMessageStatusFormatter.STATUS_DELIVERED)
                             .build();
 
-                    connectServerSendText(mChatView.getInputText());
+                    sendTextToServer(mChatView.getInputText());
                     //Set to chat view
                     mChatView.send(message);
                     //Reset edit text
@@ -169,7 +124,7 @@ public class MessengerActivity extends Activity {
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if (status != ERROR) {
+                if(status != ERROR) {
                     // 언어를 선택한다.
                     tts.setLanguage(Locale.KOREAN);
                 }
@@ -178,23 +133,22 @@ public class MessengerActivity extends Activity {
         tts.setPitch(1.0f);
         tts.setSpeechRate(1.0f);
 
-
-        SttIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        SttIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getApplicationContext().getPackageName());
-        SttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");//한국어 사용
-        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        SttIntent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        SttIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getApplicationContext().getPackageName());
+        SttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");//한국어 사용
+        mRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
         mRecognizer.setRecognitionListener(listener);
 
         //Click option button
         mChatView.setOnClickOptionButtonListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MessengerActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MessengerActivity.this,new String[]{Manifest.permission.RECORD_AUDIO},1);
                 } else {
-                    try {
+                    try{
                         mRecognizer.startListening(SttIntent);
-                    } catch (Exception e) {
+                    } catch(Exception e) {
                         e.printStackTrace();
                     }
                 }
@@ -215,7 +169,6 @@ public class MessengerActivity extends Activity {
 
         @Override
         public void onRmsChanged(float v) {
-
         }
 
         @Override
@@ -232,8 +185,7 @@ public class MessengerActivity extends Activity {
 
         @Override
         public void onResults(Bundle results) {
-            String key= "";
-            key = SpeechRecognizer.RESULTS_RECOGNITION;
+            String key= SpeechRecognizer.RESULTS_RECOGNITION;
             ArrayList<String> mResult =results.getStringArrayList(key);
             String[] rs = new String[mResult.size()];
             mResult.toArray(rs);
@@ -250,7 +202,7 @@ public class MessengerActivity extends Activity {
                         .setStatusStyle(Message.Companion.getSTATUS_ICON())
                         .setStatus(MyMessageStatusFormatter.STATUS_DELIVERED)
                         .build();
-                connectServerSendText(rs[0]);
+                sendTextToServer(rs[0]);
                 mChatView.send(message);
                 count=0;
             }else
@@ -286,8 +238,8 @@ public class MessengerActivity extends Activity {
         //User icon
         Bitmap myIcon = BitmapFactory.decodeResource(getResources(), R.drawable.face_2);
         //User name
-        SharedPreferences sharedPreferences = getSharedPreferences("SHARE_PREF", MODE_PRIVATE);
-        String myName = sharedPreferences.getString("Name", "NoName");
+        SharedPreferences sharedPreferences = getSharedPreferences("SHARE_PREF",MODE_PRIVATE);
+        String myName = sharedPreferences.getString("Name","NoName");
 
         int yourId = 1;
         Bitmap yourIcon = BitmapFactory.decodeResource(getResources(), R.drawable.chatbotimg);
@@ -306,12 +258,12 @@ public class MessengerActivity extends Activity {
         initUsers();
     }
 
-    public void connectServerSendText(String texts){
-        String postUrl = "http://" + ipv4Address + ":" + portNumber + "/webhook";
+    public void sendTextToServer(String texts){
+        String postUrl = httpConn.getUrl("webhook");
         RequestBody formBody = new FormBody.Builder()
                 .add("message", texts)
                 .build();
-        postRequest(postUrl, formBody);
+        postRequest(postUrl,formBody);
     }
 
     void postRequest(String postUrl, RequestBody postBody) {
@@ -327,7 +279,7 @@ public class MessengerActivity extends Activity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(context, "Failed to Connect to Server. Please Try Again.", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Failed to Connect to Server. Please Try Again.", Toast.LENGTH_LONG).show();
                     }
                 });
             }
@@ -342,17 +294,15 @@ public class MessengerActivity extends Activity {
                             res = response.body().string();
                         } catch (IOException e) {
                             e.printStackTrace();
-                            Toast.makeText(context, "연결 오류가 발생되어 응답을 받지 못했습니다.", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "연결 오류가 발생되어 응답을 받지 못했습니다.", Toast.LENGTH_LONG).show();
                         }
                         if(!res.equals("")) {
                             receiveMessage(res);
-                            tts.speak(res, TextToSpeech.QUEUE_FLUSH, null, null);
+                            tts.speak(res,TextToSpeech.QUEUE_FLUSH,null,null);
                         }
                     }
                 });
-
             }
         });
     }
-
 }
