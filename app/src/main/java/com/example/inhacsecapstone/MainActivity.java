8874d8dacfee5e2dd29765c62aec70fee6ab265a
tiramisu <minjun13795@naver.com>
2020-06-13 21:23:08 +0900

    + 테스트

--- app/src/main/java/com/example/inhacsecapstone/MainActivity.java
+++ app/src/main/java/com/example/inhacsecapstone/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.Animation;
 
 import androidx.annotation.NonNull;
@@ -88,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         // Bottom Navigation View Setting
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new Calendars()).commit();
         //
     }
